cebfb4d2e11d4fcad07b344c3aee96c46ad8b77e
Ishaq Usman <ishaquusmanu3@gmail.com>
2024-07-28 12:38:20 +0100

    contatct fixed

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -11,14 +11,14 @@ export const Contact = () => {
       <div className="contact">
         <img className='pic-logo'src="https://i.ibb.co/Hxfq5qz/ishaq.jpg" alt="" />
         <p className="name">Ishaq Usman</p>
-        <p className='address '>Department of Software Engineering, <br />Faculty of Computing, <br /> Bayero University Kano.</p>
+        <p className='address '>ishaquusmanu3@gmail.com || +2348129471676 <br />Department of Software Engineering, <br />Faculty of Computing, <br /> Bayero University Kano.</p>
         <div className="icons">
           <a href='http://wa.me/+2348129471676' target='blank' ><FaWhatsapp className="icon" /></a>
-          <a href='http://wa.me/+2348129471676' target='blank' ><FaFacebook className="icon" /></a>
+          <a href='http://www.facebook.com/BiinUthmaan' target='blank' ><FaFacebook className="icon" /></a>
           <a href="http://hithub.com/ishaquusmanu3"target='blank'><BsGithub className="icon"/></a>
-          <a href="http://twitter.com/ghost"target='blank'><BsTwitterX className="icon"/></a>
-          <a href="http://instagram.com"target='blank'><BsInstagram className="icon"/></a>
-          <a href="http://linkedin/ishaquusmanu3"target='blank'><BsLinkedin className="icon"/></a>
+          <a href="http://x.com/N_Ghostz?s=09"target='blank'><BsTwitterX className="icon"/></a>
+          <a href="http://instagram.com/ishaqibnuthman"target='blank'><BsInstagram className="icon"/></a>
+          <a href="https://www.linkedin.com/in/%20ishaq-usman-4b0487293"target='blank'><BsLinkedin className="icon"/></a>
         </div>
       </div>
     </div>
